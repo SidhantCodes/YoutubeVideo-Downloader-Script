2c6991ed86e457ee6a839d235a68f629f31f8870
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-10-01 11:09:52 +0530

    Completed the Script

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ from pytube import YouTube
 
 link = input("Enter the link of the YouTube video to be downloaded: ")
 
+yt = YouTube(link)
+
+print("Title: ", yt.title, "\nViews: ", yt.views, "\nRating: ", yt.rating)
+
+
+yd = yt.streams.get_highest_resolution();
+#can even just extract the audio
+# ya = yt.streams.get_audio_only();
+
+yd.download('C:/Users/Sidhant Mishra/Downloads')
+# ya.download('C:/Users/Sidhant Mishra/Downloads')
+
+
