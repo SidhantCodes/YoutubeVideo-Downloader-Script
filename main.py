a8ddf1542df4d5dfe386143be8eca1a9e06eb461
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-10-17 01:24:31 +0530

    update

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ yd = yt.streams.get_highest_resolution();
 #can even just extract the audio
 # ya = yt.streams.get_audio_only();
 
-yd.download('Enter the path where you want to download the video')
+if yd:
+    yd.download('Enter the path where you want to download the video')
+else:
+    print("Couldn't find a stream of the highest resolution.")
 # ya.download('Enter the path where you want to download the audio')
 
 
